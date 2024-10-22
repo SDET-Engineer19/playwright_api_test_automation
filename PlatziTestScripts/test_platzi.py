4e8ce3846a24354a245d08e627a7a2ebad785f88
AutomationTester19 <Justypedigvijay@gmail.com>
2024-10-22 16:42:42 +0530

    updated logs

--- PlatziTestScripts/test_platzi.py
+++ PlatziTestScripts/test_platzi.py
@@ -10,7 +10,6 @@ def test_get_product_api_details(api_request_context: APIRequestContext) -> None
     product_response = api_request_context.get(url=EndPoints.get_all_products_endpoint, headers=product_headers)
     assert product_response.status == 200
     response = product_response.json()
-    log.logger.info("\n")
     log.logger.info("Get Product API Response:\n{}".format(response))
 
 
@@ -30,9 +29,7 @@ def test_create_new_product(api_request_context: APIRequestContext) -> None:
                                                 data=product_body)
     assert product_response.status == 201
     response = product_response.json()
-    print("\n")
-    # log.logger.info("New Product Response:\n".format(product_response.json()))
-    print("New Product Response:\n{}".format(response))
+    log.logger.info("New Product Response:\n".format(product_response.json()))
 
     assert response.get("title") == product_body.get("title")
     assert response.get("price") == product_body.get("price")
